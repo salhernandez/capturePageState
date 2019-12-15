b6d0d2ca9b12478b9985a49e1ec1a1fe4e419c59
Salvador Hernandez <hernandezg.sal@gmail.com>
2019-12-14 17:41:26 -0800

    inject js when page loads (#3)

---
+++ payload.js
@@ -0,0 +1,29 @@
+console.log("bruh we made it")
+
+// remember original console.log
+var origLog = console.log;
+// here, the contents of console.log will be stored
+var consoleBuffer = [];
+
+// replace console.log with own function
+console.log = function () {
+   var args = Array.prototype.slice.call(arguments);
+   
+   // I would not store an infinite amount of entries,
+   // so remove oldest one if 10 stored
+   if (consoleBuffer.length === 10) consoleBuffer.pop();
+
+   // remember
+   consoleBuffer.push(args);
+
+   // call original function
+   origLog.apply(console, args);
+
+   //    send info (arguments back to popup.html/js via message). 
+};
+
+console.log('foo');
+var err = 'unknown error';
+console.log('An error occured: ', err);
+// use warn since this was not replaced
+console.warn(consoleBuffer);
