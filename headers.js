3ff7d3946b9f269fc8eb6b8453209975b0c35bfa
Sal Hernandez <hernandezg.sal@gmail.com>
2019-12-25 15:09:38 -0800

    Can download console logs

--- headers.js
+++ headers.js
@@ -4,11 +4,25 @@
 
 var tabId = parseInt(window.location.search.substring(1));
 
+var data = [];
+
 window.addEventListener("load", function () {
 
     // use Log.enable and go from there
     chrome.debugger.sendCommand({ tabId: tabId }, "Log.enable");
     chrome.debugger.onEvent.addListener(onEvent);
+
+
+    setTimeout(() => {
+        let harBLOB = new Blob([JSON.stringify(data)]);
+
+        let url = URL.createObjectURL(harBLOB);
+
+        chrome.downloads.download({
+            url: url
+        });
+    }, 3000);
+
 });
 
 window.addEventListener("unload", function () {
@@ -24,6 +38,7 @@ function onEvent(debuggeeId, message, params) {
 
     var requestDiv = document.createElement("div");
     var urlLine = document.createElement("div");
+    data.push(params)
     urlLine.textContent = JSON.stringify(debuggeeId) +"\n"+JSON.stringify(message) +"\n"+ JSON.stringify(params)+"\n";
     requestDiv.appendChild(urlLine);
     document.getElementById("container").appendChild(requestDiv);
